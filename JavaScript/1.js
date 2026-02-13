6dd93784d93f86e03ea5ff65ef5d9c5a675a7f11
rockabhishekak <abhishek.kumar20248@lpu.in>
2026-02-13 12:39:09 +0530

    javaScript folder

---
+++ JavaScript/1.js
@@ -0,0 +1,10 @@
+console.log("hello");
+console.log('hello');
+alert("Hi");
+// Variables are ccontainers for data.
+fullName = "abhishek";
+console.log(fullName);
+age = 12;
+console.log(age);
+price = 99.99;
+console.log(price);
---
+++ JavaScript/datatype.js
@@ -0,0 +1,6 @@
+const student{
+    name: "Abhishek";
+    age: 15;
+    cgpa: 8.51;
+}
+console.log(student["cgpa"]);
