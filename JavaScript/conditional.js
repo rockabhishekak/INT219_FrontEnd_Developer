ad7e94001bb94ac1a3fe32b0959255e64f60a410
rockabhishekak <abhishek.kumar20248@lpu.in>
2026-02-16 11:33:13 +0530

    conditianal js and grade system

--- JavaScript/conditional.js
+++ JavaScript/conditional.js
@@ -20,3 +20,29 @@ else{
     color = "white";
 }
 console.log(color);
+// Ternary operators 
+result = age >=18 ? "adult":"not adult";
+console.log(result);
+age >= 18 ? console.log("adult"):console.log("not adult");
+
+// Switch case
+let day=2;
+switch(day){
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+    default:
+        console.log("Weekend");
+        break;
+}
---
+++ JavaScript/grade.js
@@ -0,0 +1,30 @@
+let num = 68;
+let grade;
+if(num>=90 && num<=100{
+    grade = "O";
+}
+else if(num >= 80 && num <= 89)
+{
+    grade = "A+";
+}
+else if(num >= 70 && num <= 79)
+{
+    grade = "A";
+}
+else if(num >= 60 && num <= 69)
+{
+    grade = "B+";
+}
+else if(num >= 50 && num <= 59)
+{
+    grade = "B";
+}
+else if(num >=40 && num <= 49)
+{
+    grade = "C";
+}
+else
+{
+    grade = "F";
+}
+
