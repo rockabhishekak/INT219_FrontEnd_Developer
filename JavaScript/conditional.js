2699a9c1da94a96d614ee116ed450a7334d481c4
rockabhishekak <abhishek.kumar20248@lpu.in>
2026-02-16 10:49:12 +0530

    conditional js and oprators js

---
+++ JavaScript/conditional.js
@@ -0,0 +1,22 @@
+let age = 18;
+if(age >= 18)
+{
+    console.log("you can vote");
+}
+else{
+    console,log("Yor can't vote");
+}
+
+let mode = "light";
+let color;
+if(mode === "dark")
+{
+    color = "black";
+}
+else if(mode === "blue"){
+    color = "blue";
+}
+else{
+    color = "white";
+}
+console.log(color);
---
+++ JavaScript/oprators.js
@@ -0,0 +1,31 @@
+let a = 5;
+let b = 2;
+let c = a+b;
+console.log(c);
+console.log("++a = ",++a);
+console.log("a++ = ",a++);
+console.log("a = ",a);
+console.log("--a = ",--a);
+console.log("a-- = ",a--);
+console.log("a = ",a);
+
+// Assignment Operators (right to left):
+// =,+=,-=,*=,%=,
+a+=4;
+console.log("a = ",a);
+
+// Comparision Operators: 
+let x = 5; //number
+let y = "5";
+console.log("x==y ",x==y);//true
+console.log("x===y ",x===y);//false
+console.log("x!=y ",x!=y);//false
+console.log("x!==y ",x!==y)//true
+let p = 5;
+let r = 3;
+let con = p>r;
+let cn = y ===5;
+console.log("cond1 && cond2 = ",con && cn);
+console.log("Cond1 || cond2",pn || cn);
+console.log(p&&r);
+
