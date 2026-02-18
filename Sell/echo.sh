c8781bd2a959052b1e3646efdaa8ebf29bf9d1d9
rockabhishekak <abhishek.kumar20248@lpu.in>
2026-02-18 14:56:18 +0530

    stack shell script

--- Sell/echo.sh
+++ Sell/echo.sh
@@ -1,3 +1,48 @@
-echo "abhi"
-read snum
-echo $num
+top=-1
+declare a -stack
+push(){
+    ((top++))
+    stack[$top]=$1
+    echo "Element added: $1"
+
+}
+pop(){
+    if ((top==-1))
+    then 
+        echo "under flow"
+    else
+        echo "Element deleted: ${stack[$top]}"
+        ((top--))
+    fi
+}
+display(){
+    if((top==-1))
+    then
+        echo "Empty"
+    else 
+        for((i=top;i>=0;i--))
+        do
+            echo ${stack[$i]}
+        done
+    fi
+}
+while true
+do
+    echo "1.push , 2 pop 3 Display, 4 Exit"
+    read ch
+    case $ch in 
+    1)
+        echo "Enter element"
+        read val
+        push $val
+        ;;
+    2)
+     pop ;;
+    3)
+    display ;;
+    4)
+    break ;;
+    *)
+    echo "invalid iput"
+    esac
+done
