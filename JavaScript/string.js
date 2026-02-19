66e0962e1e48dc89f3a87b391f9a7ddd6e063e16
rockabhishekak <abhishek.kumar20248@lpu.in>
2026-02-19 14:32:05 +0530

    add string.js

---
+++ JavaScript/string.js
@@ -0,0 +1,35 @@
+let str = "Hello world"
+let newstr=str.toUpperCase();
+console.log(str);//original string 
+console.log(newstr)//new string
+let newstrr = str.toLowerCase()
+console.log(newstrr);
+
+
+let str2 = "    hi all   ";
+console.log(str2.trim());
+
+//slice methode
+let str3="Hello"
+console.log(str3.slice(0,3));
+console.log(str3.slice(1));
+
+let stri = "hello";
+let strii = "world";
+let res = stri.concat(strii);
+console.log(res);
+let  resi = strii.concat(stri);
+console.log(resi);
+
+let stri3  = "hello";
+console.log(stri3.replace("l","m"));
+console.log(stri3.replaceAll("lo","p"));
+
+
+// let str= "Hi";
+// str //write on console
+// str.length//write on console
+// console.log(str[0]);
+// let str2 = "hii";
+// str2.length
+// console.log(str[1]);
