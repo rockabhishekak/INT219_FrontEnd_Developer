5cdf711caf337460a3248c4ddf66c18e2bcca0a5
rockabhishekak <abhishek.kumar20248@lpu.in>
2026-02-21 10:24:00 +0530

    array and prompt task 3

---
+++ JavaScript/array.js
@@ -0,0 +1,8 @@
+let marks = [97,82,75,64,36];
+console.log(marks);
+console.log(marks.length)//property
+typeof marks; // object type
+console.log(marks[0]);
+console.log(marks[2]);
+console.log(marks[5]); // undefined
+marks[2] = 76;
---
+++ JavaScript/prompt.js
@@ -0,0 +1,10 @@
+let num = prompt("Enter a number: ");
+if(num % 5 === 0)
+{
+    console.log(num,"is multi of 5");
+}
+else
+    {
+    console.log(num, "is multi not of 5");
+
+}
