74285f9be5cabc8ceb7ac6fb4eaa648ac2a2c095
rockabhishekak <abhishek.kumar20248@lpu.in>
2026-02-26 15:26:07 +0530

    Dom modification

--- JavaScript/DOM.js
+++ JavaScript/DOM.js
@@ -1,9 +1,3 @@
-/*
-
-
-*/
-
-
 window //type on consle to see all the properties & methods of window object 
 window.console.log("Hello World") //to print something on console
 /* DOM -> Document Object Model -> window -> Document -> HTML Elements  -> Head & Body -> All the tags in HTML are called as HTML Elements */
@@ -11,19 +5,25 @@ window.document //to access the document object
 console.dir(window.document) //to see all the properties & methods of document object
 console.log(document.head) //to access head element
 console.log(document.body) //to access body element
-console.log(document.title) //to access title of the document
 // Dom manipulation -> changing the content of the document using JavaScript
 // console.dir(document.body.style.backgroundColor = "red")
 
 
 // select an element by its id
-document.getElementById("heading");
-console.dir(heading);
-document.getElementById("chinmay");
-console.dir();
+let headi = document.getElementById("head");
+console.dir(headi);
+let tag = document.getElementsByTagName("h1");
+console.dir(tag);
+
 // if id does not exist, it will return null
 
 // select a element by its class 
+let headings = document.getElementsByClassName("heading-class");
+console.log(headings);
+console.dir(headings);
 
-// document.getElementsByClassName("heading-class");
-// console.dir(heading-Class);
+let firstele = document.querySelector("p"); //it will select the first element with the tag name "p"
+console.log(firstele);
+
+let allclasses = document.querySelectorAll(".heading-class"); //it will select all the elements with the class name "heading-class"
+console.log(allclasses);
