53f8250dce70ad06296f2965f0a7d50d0fee8eae
rockabhishekak <abhishek.kumar20248@lpu.in>
2026-02-26 14:50:06 +0530

    Doms

--- JavaScript/DOM.js
+++ JavaScript/DOM.js
@@ -1,3 +1,9 @@
+/*
+
+
+*/
+
+
 window //type on consle to see all the properties & methods of window object 
 window.console.log("Hello World") //to print something on console
 /* DOM -> Document Object Model -> window -> Document -> HTML Elements  -> Head & Body -> All the tags in HTML are called as HTML Elements */
@@ -6,5 +12,18 @@ console.dir(window.document) //to see all the properties & methods of document o
 console.log(document.head) //to access head element
 console.log(document.body) //to access body element
 console.log(document.title) //to access title of the document
-console.log(document.URL) //to access URL of the document
-console.log(document.domain) //to access domain of the document
+// Dom manipulation -> changing the content of the document using JavaScript
+// console.dir(document.body.style.backgroundColor = "red")
+
+
+// select an element by its id
+document.getElementById("heading");
+console.dir(heading);
+document.getElementById("chinmay");
+console.dir();
+// if id does not exist, it will return null
+
+// select a element by its class 
+
+// document.getElementsByClassName("heading-class");
+// console.dir(heading-Class);
