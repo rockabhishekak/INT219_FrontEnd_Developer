dbb66c47a7f212da13f16c90d29bb7c2e7b127b4
rockabhishekak <abhishek.kumar20248@lpu.in>
2026-03-04 21:01:14 +0530

    DOM3

---
+++ JavaScript/DOM3.js
@@ -0,0 +1,31 @@
+/* Attributes*/
+// 1) getAttribute(attr) - to get the value of an attribute
+let div = document.querySelector('div');
+console.log(div);
+let id = div.getAttribute('id');
+console.log(id);
+let name = div.getAttribute('name');
+console.log(name);
+let para = document.querySelector('p');
+let className = para.getAttribute('class');
+console.log(className);
+
+// 2) setAttribute(attr, value) - to set the value of an attribute
+// let para1 = document.querySelector('p');
+// console.log(para1.setAttribute('class', 'newClass'));
+
+
+/* Style*: node.style */
+
+// let div = document.querySelector('div');
+// console.log(div);
+// div.style//inline style object (type on console)
+// div.style.backgroundColor = 'green';
+// div.style.width = '200px';
+// div.style.height = '200px';
+// div.style.border = '2px solid red';
+// div.style.color = 'pink';
+// div.innerText = 'Styled Div';
+
+
+
