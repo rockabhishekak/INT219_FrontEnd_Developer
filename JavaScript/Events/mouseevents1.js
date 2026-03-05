151b08d436fb9b3eef3d1479b74404b576adade8
rockabhishekak <abhishek.kumar20248@lpu.in>
2026-03-05 15:40:43 +0530

    Dom 4 and Events

---
+++ JavaScript/Events/mouseevents1.js
@@ -0,0 +1,7 @@
+// Events - > 
+// The chagne in the state of an element is called an event.
+// Events are fired to notify the occurrence of an action.
+// 1)Mouse events(click, double click , mouseover, mouseout, mouseenter, mouseleave, mousedown, mouseup)
+// 2)Keyboard events(keydown, keyup, keypress)
+// 3)Form events(submit, change, focus, blur)
+// 4) print events(load, unload, resize, scroll)
