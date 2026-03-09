949f92c00db11d85fb89e1f99efbfb1e1cc1794b
rockabhishekak <abhishek.kumar20248@lpu.in>
2026-03-09 11:32:10 +0530

    using methode two and one

--- JavaScript/Events/ME3.js
+++ JavaScript/Events/ME3.js
@@ -21,3 +21,33 @@ btn1.addEventListener("click",()=>{
 //     console.log("Button 1 clicked handler4");
 // });
 btn1.removeEventListener("click",handler3); //This will work because the reference of the callback is same
+
+// Q1 :
+// Create a toggle button that changes the screen to dark- mode then
+// clicked & light - mode when clicked again. (Hint: use classList.toggle() method)
+// let toggleBtn = document.getElementById("toggleBtn");
+// toggleBtn.addEventListener("click", () => {
+//     document.body.classList.toggle("dark-mode");
+//     // Conditional statement to change the button text based on the current mode
+//     // if (document.body.classList.contains("dark-mode")) {
+//     //     toggleBtn.textContent = "Switch to Light Mode";
+//     // } else {
+//     //     toggleBtn.textContent = "Switch to Dark Mode";
+//     // }
+
+// });
+
+// Methode 2
+let toggleBtn2 = document.getElementById("toggleBtn");
+let count = 0;
+toggleBtn2.addEventListener("click", () => {
+    if (count % 2 == 0) {
+        document.body.style.backgroundColor = "black";
+        document.body.style.color = "white";
+        count++;
+    } else {
+        document.body.style.backgroundColor = "white";
+        count++;
+        document.body.style.color = "black";
+    }
+});
