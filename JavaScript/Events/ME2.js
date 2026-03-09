df3ddea5018f0928fa845c5aa4f5744d49eb4272
rockabhishekak <abhishek.kumar20248@lpu.in>
2026-03-09 10:14:55 +0530

    Modify the mouse events 2

---
+++ JavaScript/Events/ME2.js
@@ -0,0 +1,23 @@
+// Js event handling
+// node.event = (function)=>{})
+// 1)inline event handling
+// 2)event handler as a property of an element
+// 3)event listener using addEventListener() method
+
+let btn1 = document.querySelector("#btn1");
+btn1.onclick = () => {
+    console.log("button was clicked");
+    let a = 25;
+    a++;
+    console.log(a);//26
+}; //event handler as a property of an element
+
+let div = document.querySelector("div");
+div.onmouseover = () =>{
+    console.log("mouse is over the div");
+}
+
+// if we have done inline event handling and event handler as well as js evenet handling both together,
+// then the event handler as a property of an element will override the inline event handling and only the event handler as a property of an element will work.
+// if we have used any event suppose onclick event used oncuechange, the you cannot use it again 
+// if you want to again then the new one will overwite.
